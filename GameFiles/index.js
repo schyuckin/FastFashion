b468dad3f403edd866e80b7eb9c27e58d83f5f10
Kostya Deputatov <kostyadeputatov@gmail.com>
2023-01-20 18:31:33 +0100

    Basic movement set-up

---
+++ GameFiles/index.js
@@ -0,0 +1,68 @@
+ const canvas = document.querySelector('canvas')
+ const c = canvas.getContext("2d")
+
+ canvas.width = window.innerWidth
+ canvas.height = window.innerHeight
+
+ const gravity = 0.5;
+
+ class Player {
+    constructor(){
+         this.position = {
+            x:100,
+            y:100
+         }
+
+         this.velocity = {
+            x: 0,
+            y: 0
+
+         }
+         this.width = 30
+         this.height = 30
+    }
+    
+    draw(){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update(){
+        this.draw()
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+        this.velocity.y += gravity
+        else this.velocity.y = 0
+    }
+ }
+
+ const player = new Player()
+
+ function animate(){
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+ }
+
+ animate()
+
+ window.addEventListener('keydown', ({keyCode}) => {
+      switch (keyCode){
+        case 65:
+            console.log('left')
+            break
+
+        case 83:
+            console.log('down')
+            break
+
+        case 68:
+            console.log('right')
+            break
+
+        case 87:
+            console.log('up')
+            break  
+      }
+ } )
